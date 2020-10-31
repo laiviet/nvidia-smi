03c630ec42aa0a65af25a5786308b86f49c9c0e1
Viet Lai <laiviet2811@gmail.com>
2020-10-31 04:05:58 -0700

    Update compile.h

--- compile.sh
+++ compile.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-/usr/local/cuda/bin/nvcc  --output-file nvidia-smi ./nvidia-smi.cpp
+
+if [[ -f /usr/local/cuda/bin/nvcc ]] ; then
+  /usr/local/cuda/bin/nvcc  --output-file nvidia-smi ./nvidia-smi.cpp
+else
+  nvcc --output-file nvidia-smi ./nvidia-smi.cpp
+fi
+
 
 ./nvidia-smi
